13bee33550c05b9bed4a886225d7d4a893021516
Muhammad <muhannmmad@gmail.com>
2024-10-02 15:41:15 +0200

    changed only to class

--- aufgabe_2_october/lib/profile.dart
+++ aufgabe_2_october/lib/profile.dart
@@ -1,21 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyApp',
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green)),
-      home: ProfileScreen(),
-    );
-  }
-}
-
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -56,22 +40,6 @@ class ProfileScreen extends StatelessWidget {
               color: Colors.black,
               fontWeight: FontWeight.w500,
             ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: 'News',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Likes',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Profile',
           ),
         ],
       ),
