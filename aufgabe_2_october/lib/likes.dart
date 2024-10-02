a83b0b719d75e509d5a8f3fa4272869e03af2a06
Muhammad <muhannmmad@gmail.com>
2024-10-02 13:56:25 +0200

    changes made to be page likes

--- aufgabe_2_october/lib/likes.dart
+++ aufgabe_2_october/lib/likes.dart
@@ -34,27 +34,27 @@ class ProfileScreen extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
-            'Profile',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
+          Center(
+            child: SizedBox(
+              height: 30,
+              child: Text(
+                'Likes',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
             ),
           ),
-          Center(child: SizedBox(height: 20)),
-          Icon(
-            Icons.account_circle,
-            size: 80,
-            color: Colors.black,
-          ),
-          SizedBox(height: 10),
-          Text(
-            'Max Mustermann',
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.black,
-              fontWeight: FontWeight.w500,
+          Center(
+            child: Text(
+              'Hier findest du deine gelikten Nachrichten',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
             ),
           ),
         ],
